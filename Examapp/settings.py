30eeb51956162682c9536b35268af278ddcd0e57
Arnav0204 <anjoglekar0204@gmail.com>
2023-07-30 18:37:51 +0530

    new attempt 4

--- Examapp/settings.py
+++ Examapp/settings.py
@@ -30,6 +30,15 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
 
 # Application definition
 
